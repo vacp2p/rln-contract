0b287c40c204ccfd33de550705d2df815146d1dc
rymnc <43716372+rymnc@users.noreply.github.com>
2022-11-24 12:09:44 +0530

    feat(rln): store valid group ids from interep

---
+++ contracts/ValidGroupStorage.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@interep/contracts/Interep.sol";
+
+contract ValidGroupStorage {
+    mapping(uint256 => bool) public validGroups;
+
+    Interep public interep;
+
+    struct Group {
+        bytes32 provider;
+        bytes32 name;
+    }
+
+    constructor(address _interep, Group[] memory _groups) {
+        interep = Interep(_interep);
+        for (uint8 i = 0; i < _groups.length; i++) {
+            uint256 groupId = uint256(
+                keccak256(
+                    abi.encodePacked(_groups[i].provider, _groups[i].name)
+                )
+            ) % SNARK_SCALAR_FIELD;
+            (bytes32 provider, bytes32 name, , ) = interep.groups(groupId);
+            if (provider == _groups[i].provider && name == _groups[i].name) {
+                validGroups[groupId] = true;
+            } else {
+                revert("[ValidGroupStorage] Invalid group");
+            }
+        }
+    }
+
+    function isValidGroup(uint256 _groupId) public view returns (bool) {
+        return validGroups[_groupId];
+    }
+}
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -23,7 +23,13 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.15",
+  solidity: {
+    compilers: [{
+      version: "0.8.4",
+    }, {
+      version: "0.8.15"
+    }],
+  },
   networks: {
     goerli: {
       url: GOERLI_URL,
